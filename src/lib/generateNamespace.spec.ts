99dcbb0a32a3273150358909100edb4d55543ddf
Pon Cheol Ku (구본철) <bbon@live.com>
2022-10-22 12:22:43 +0900

    refactor: Unittest #1

--- src/lib/generateNamespace.spec.ts
+++ src/lib/generateNamespace.spec.ts
@@ -2,19 +2,21 @@ import * as path from "path";
 import * as assert from "assert";
 import generateNamespace from "./generateNamespace";
 
-describe("generateNamespace", () => {
-  it("should generate namespace with project file name and their directory structure", () => {
-    const projectFilePath = path.join(
-      process.cwd(),
-      "forTest/Type1/Sample.csproj"
-    );
-    const currentFilePath = path.join(process.cwd(), "forTest/Type1/Models");
+describe("lib", () => {
+  describe("# generateNamespace", () => {
+    it("should generate namespace with project file name and their directory structure", () => {
+      const projectFilePath = path.join(
+        process.cwd(),
+        "forTest/Type1/Sample.csproj"
+      );
+      const currentFilePath = path.join(process.cwd(), "forTest/Type1/Models");
 
-    const generatedNamespace = generateNamespace(
-      projectFilePath,
-      currentFilePath
-    );
+      const generatedNamespace = generateNamespace(
+        projectFilePath,
+        currentFilePath
+      );
 
-    assert.equal(generatedNamespace, "Sample.Models");
+      assert.equal(generatedNamespace, "Sample.Models");
+    });
   });
 });
